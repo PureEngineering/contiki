7d1e3f80ac9cfcb0de31eb6e470a814cc186e7dd
PureF <furqaan@pureengineering.com>
2017-02-07 13:17:53 -0800

    Database Change

--- examples/canary/python/database.py
+++ examples/canary/python/database.py
@@ -6,11 +6,16 @@ class Database:
 		self.table = t
 		self.mariadb_connection = mariadb.connect(user=u, password=p, database=d)
 		self.cursor = self.mariadb_connection.cursor()
-		create = "CREATE TABLE IF NOT EXISTS " + self.table + " (ID INTEGER PRIMARY KEY AUTO_INCREMENT, uniq_id  INTEGER NOT NULL, time INTEGER NOT NULL, type TEXT NOT NULL, data BLOB NOT NULL)"
+		create = """ CREATE TABLE IF NOT EXISTS """ + self.table + """ 
+			     (ID INTEGER PRIMARY KEY AUTO_INCREMENT, 
+			     uniq_id  INTEGER NOT NULL, time INTEGER NOT NULL, 
+			     type TEXT NOT NULL, data BLOB NOT NULL) """
 		self.cursor.execute(create)
 	
 	def insert(self, id, time, type, data):
-		query = "INSERT INTO " + self.table + " (uniq_id, time, type, data) VALUES (%s, %s, %s, %s)"
+		query = """ INSERT INTO """ + self.table + """
+			    (uniq_id, time, type, data) 
+			    VALUES (%s, %s, %s, %s) """
 		args = (id, time, type, data)
 		self.cursor.execute(query, args)
 		self.mariadb_connection.commit()
