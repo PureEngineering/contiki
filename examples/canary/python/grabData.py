507eae5d2f0ebcc25a2052c1eef84492e9535d1c
PureF <furqaan@pureengineering.com>
2017-04-16 19:30:17 -0700

    Make Changes To reading data

--- examples/canary/python/grabData.py
+++ examples/canary/python/grabData.py
@@ -3,7 +3,9 @@ import sys
 
 def main():
 	db = Database("newuser", "secretpassword", str(sys.argv[1]))
-	db.read()
+	sensorData = db.read()
+	for k in sensorData:
+		print("{},{},{},{}").format(sensorData[k][0], sensorData[k][1], sensorData[k][2], sensorData[k][3])
 
 if __name__ == "__main__":
 	main()
