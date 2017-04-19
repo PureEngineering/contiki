812c442bf44b222a13b29f01377ea7f5545bce23
PureF <furqaan@pureengineering.com>
2017-04-19 16:18:42 -0700

    .

--- examples/canary/python/readSerial.py
+++ examples/canary/python/readSerial.py
@@ -1,6 +1,11 @@
 import serial
 import sensor_message_pb
 import base64
+from database import Database
+import sys
+import time
+
+db = Database("newuser", "secretpassword", str(sys.argv[1]))
 
 message = sensor_message_pb.sensors()
 
@@ -12,11 +17,25 @@ serialPort = '/dev/ttyUSB0'
 ser = serial.Serial(port = serialPort, baudrate = baudRate, timeout = 1)
 while ser.is_open:
 	byte = ser.readline()
+	#byte = ser.read()
 	if byte == '':
 		print 'No Data'
 	#elif byte == '\n':
+		#arr = base64.b64encode(arr)
+		#message.ParseFromString(arr)
 		#print arr
 		#arr = ""
 	else:
 		#arr += byte
-		print byte
+		length =  len(byte[2:-1])
+		#print int(byte[:-2])
+		#print ("len %d", length)
+		#print byte[0] + byte[1]
+		print length
+		if str(length) == str(byte[0] + byte[1]):
+			#print "HI"
+			#print length
+			print byte[2:-1]
+			message.ParseFromString(byte[2:-1])
+			milli = (int(round(time.time()*1000)))
+			db.insert(message.id, milli, "Canary", byte[2:-1])
