9f99ab0770d64bc7b9e1cce9d14d6190a55cf89e
PureF <furqaan@pureengineering.com>
2017-04-26 14:34:06 -0700

    .

--- examples/canary/python/readSerial.py
+++ examples/canary/python/readSerial.py
@@ -3,28 +3,49 @@ import sensor_message_pb
 from database import Database
 import sys
 import time
+#import thread
 
-db = Database("newuser", "secretpassword", str(sys.argv[1]))
+def readSerial(db, message, ser, one):
+	while ser.is_open:
+		byte = ser.readline()
+		if byte == '':
+			print 'No Data'
+		else:
+			payload = byte[1:-1]
+			length =  len(payload)
+			lenBytes = ord(byte[0])
+			print length
+			if length > 0 and length == lenBytes:
+				print payload
+				message.ParseFromString(payload)
+				milli = (int(round(time.time()*1000)))
+				db.insert(message.id, milli, "Canary", payload)
+				if (one):
+					break
 
-message = sensor_message_pb.sensors()
+def main():
+	if(len(sys.argv) == 1):
+		print "Please Enter Database Name"
+		return
+	db = Database("newuser", "secretpassword", str(sys.argv[1]))
+	message = sensor_message_pb.sensors()
+	try:
+		baudRate = 115200
+		serialPort = '/dev/ttyUSB0'
+		ser = serial.Serial(port = serialPort, baudrate = baudRate, timeout = 1)
+	except:
+		print "Error: could not open serial port"
+		return
 
-arr = ""
+	readSerial(db, message, ser, 0)
+	#readSerial()
+	#try:
+		#thread.start_new_thread(readSerial, (ser,))
+	#except:
+		#print "Error: unable to start thread"
+	
+	#while 1:
+		#pass
 
-baudRate = 115200
-serialPort = '/dev/ttyUSB0'
-
-ser = serial.Serial(port = serialPort, baudrate = baudRate, timeout = 1)
-while ser.is_open:
-	byte = ser.readline()
-	if byte == '':
-		print 'No Data'
-	else:
-		payload = byte[1:-1]
-		length =  len(payload)
-		lenBytes = ord(byte[0])
-		print length
-		if length > 0 and length == lenBytes: #46 or length == 47 or length == 48:
-			print payload
-			message.ParseFromString(payload)
-			milli = (int(round(time.time()*1000)))
-			db.insert(message.id, milli, "Canary", payload)
+if __name__ == "__main__":
+        main()
