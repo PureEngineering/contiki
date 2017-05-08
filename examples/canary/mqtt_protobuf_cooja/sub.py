61dc0b653a9e690e2ad364000fa9026b73c1f6fc
Daniel Ng <daniel@pureengineering.com>
2017-05-08 13:50:22 -0700

    update readme

--- examples/canary/mqtt_protobuf_cooja/sub.py
+++ examples/canary/mqtt_protobuf_cooja/sub.py
@@ -1,15 +1,10 @@
 import paho.mqtt.client as mqtt
 import sensor_message_pb2
 
-# The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
     client.subscribe("c")
 
-# The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     message.ParseFromString(str(msg.payload))
     print(message)
